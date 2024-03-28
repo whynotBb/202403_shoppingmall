9c10215901ab6d3f550527165d953c18c2dadf85
kimsese <whynot.ksw@gmail.com>
2024-03-29 00:41:06 +0900

    베포오류 수정

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -16,6 +16,7 @@ const ProductDetail = () => {
     };
     useEffect(() => {
         getProducts();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
     return (
         <Container>
