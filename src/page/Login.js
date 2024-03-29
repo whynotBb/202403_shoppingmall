66d6ddc7f74c8b7a27a64445b4bf1cd44889dc44
whynot-byoul <whynot@hubilon.com>
2024-03-29 17:10:06 +0900

    배포오류 수정

--- src/page/Login.js
+++ src/page/Login.js
@@ -17,7 +17,7 @@ const Login = ({setAuthenticate}) => {
                         <Form.Group className='mb-3' controlId='formBasicEmail'>
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type='email' placeholder='Enter email' />
-                            <Form.Text className='text-muted'>We'll never share your email with anyone else.</Form.Text>
+                            <Form.Text className='text-muted'>We'll never share your email with anyone else</Form.Text>
                         </Form.Group>
 
                         <Form.Group className='mb-3' controlId='formBasicPassword'>
