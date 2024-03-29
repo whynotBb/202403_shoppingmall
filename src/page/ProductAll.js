332a8aad14822bbadc7f8ba8de78e14905c6766f
whynot-byoul <whynot@hubilon.com>
2024-03-29 17:00:02 +0900

    배포오류 수정

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -25,6 +25,7 @@ const ProductAll = () => {
     };
     useEffect(() => {
         getProducts();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [query]);
 
     const productDetail = (id) => {
